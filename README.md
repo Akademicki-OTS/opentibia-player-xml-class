de80c85c9dbb03120d65a00f7c580374ec539683
PJanisio <p.janisio@gmail.com>
2017-03-24 21:16:36 +0100

    update of functions
    core functions update
    getAccount
    getSex

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # opentibia-player-xml-class
-PHP class
+PHP class for retriving and edition of XML player file for open tibia server with XML database.
--- xml.player.php
+++ xml.player.php
@@ -1,7 +1,7 @@
 ﻿<?php
 /*
 Open Tibia XML player class
-Version: 0.0.1
+Version: 0.0.2
 Author: Pawel 'Pavlus' Janisio
 License: MIT
 Github: https://github.com/PJanisio/....
@@ -13,11 +13,13 @@ class xmlPlayer {
 
 //predefined variables
 
-public $showFullError = 1; //shows backtrace of error message //def: 1
-public $errorTxt = ''; 
-
-
-
+public $showError = 1; //shows backtrace of error message //def: 1
+public $errorTxt = ''; //placeholder for error text //def: ''
+public $playerName = '';
+public $playersDir = '';
+public $accountsDir = '';
+public $xml = NULL;
+public $account = 0;
 
 
 
@@ -27,31 +29,65 @@ public function __construct($dataPath) {
 $this->dataPath = $dataPath;
 	$this->realPath = realpath($this->dataPath);
 	
-		
-		if($this->realPath !== false AND is_dir($this->realPath)) {
-        return $this->dataPath;
-    }
-		else {
-			$this->throwError('Invalid path', 1);
-			return FALSE;
-			}
-
-
-
+		//check if this is real path and directory
+		if($this->realPath == false OR !is_dir($this->realPath)) {
+			$this->throwError('Data path invalid!', 1);
+		}
+			
+		//check if there exists player anc accounts directory	
+		if(!is_dir($this->realPath.'/players') OR !is_dir($this->realPath.'/accounts') ) {
+			$this->throwError('Players/Accounts path is invalid!', 1);
+		}
+		else
+			{
+			$this->playersDir = $this->realPath.'/players/';
+			$this->accountsDir = $this->realPath.'/accounts/';
+			}	
 
 }
 
 
 public function throwError($errorTxt, $showError) {
+			
+			
+			if($showError == 1) {
 			echo '<b>'.$errorTxt.'</b><br>';
-			
-			if($showError == 1)
-			throw new Exception($this->errorTxt);
-			
+			throw new Exception($this->errorTxt);			
 		}
+
+}
+
+
+public function prepare($playerName) {
+//function to open xml stream, not to open it every time for one player
+
+		$this->xml = @simplexml_load_file($this->playersDir.$playerName.'.xml');
 		
+			if($this->xml === FALSE) //returns not boolean false what the heck
+				$this->throwError('Player do not exists!', 1);
+				else
+					return $this->xml;
+}
+
+public function showStructure() {
+//correct
+return var_dump($this->xml);
+
+}
 
 
+public function getAccount() {
+
+return $this->xml['account'];
+
+}
+
+
+public function getSex() {
+
+return $this->xml['sex'];
+
+}
 
 }
 
